552ac133e2b6117e867aa42fd482d553de5d8598
Paul Riley <paul.riley@puppet.com>
2024-05-02 14:35:18 -0600

    fixed .gitignore after rm

--- .modules/selinux/examples/fcontext.pp
+++
@@ -1,4 +0,0 @@
-selinux::fcontext { 'set-mysql-log-context':
-  seltype  => 'mysqld_log_t',
-  pathspec => '/u01/log/mysql(/.*)?',
-}
--- .modules/selinux/examples/fcontext_equals.pp
+++
@@ -1,4 +0,0 @@
-selinux::fcontext::equivalence { '/opt/wordpress':
-  ensure => 'present',
-  target => '/usr/share/wordpress',
-}
